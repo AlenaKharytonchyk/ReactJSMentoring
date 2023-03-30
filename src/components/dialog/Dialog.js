5c3154d51f8917bcfc6caa2a6d4d4d4324d26192
AlenaKharytonchyk <alena_kharytonchyk@epam.com>
2023-03-30 21:52:27 +0300

    feat: add focus-trap-react

--- src/components/dialog/Dialog.js
+++ src/components/dialog/Dialog.js
@@ -1,6 +1,7 @@
 import React, {useState, useEffect} from "react";
 import "./Dialog.scss";
 import {createPortal} from "react-dom";
+import FocusTrap from "focus-trap-react";
 
 const Dialog = ({title, children, showModal}) => {
     const [modalVisible, setModalVisible] = useState(showModal);
@@ -11,11 +12,13 @@ const Dialog = ({title, children, showModal}) => {
     return modalVisible
         ? (
         createPortal(
-            <div className="modal-container" data-testid="dialog">
-                <div className="title">{title.toUpperCase()}</div>
-                <div className="dialog-body">{children}</div>
-                <button data-testid="close-button" className="close-button" onClick={() => setModalVisible(false)}>X</button>
-            </div>,
+            <FocusTrap>
+                <div className="modal-container" data-testid="dialog">
+                    <div className="title">{title.toUpperCase()}</div>
+                    <div className="dialog-body">{children}</div>
+                    <button data-testid="close-button" className="close-button" onClick={() => setModalVisible(false)}>X</button>
+                </div>
+            </FocusTrap>,
             document.body
         )
     )
