2498468865b67bdc83455b17522def6051d55d84
AlenaKharytonchyk <alena_kharytonchyk@epam.com>
2023-04-05 15:09:57 +0300

    feat: remove unused code

--- src/App.js
+++ src/App.js
@@ -1,50 +1,13 @@
 import './App.css';
 import {
-    Title,
-    Counter,
-    Container,
-    InputField,
-    GenreSelect,
-    MovieContainer,
-    MovieDetails,
-    SortControl,
-    MovieForm,
     MovieListPage
 } from "./components";
-import {moviesArray} from "./mockedMovies";
 
 function App() {
   return (
       <div className="App">
           <MovieListPage />
       </div>
-    // <div className="App">
-    //     <Container>
-    //       <Title title="Counter Task" />
-    //       <Counter initialValue={0} />
-    //     </Container>
-    //     <Container>
-    //         <Title title="Search field Task" />
-    //         <InputField
-    //             inputValue="What do you want to watch?"
-    //             onSearch={(value) => alert(value)}
-    //         />
-    //     </Container>
-    //     <Container>
-    //         <Title title="Genre select Task" />
-    //         <GenreSelect
-    //             genreList={["all", "comedy", "drama", "detective"]}
-    //             onSelect={(genre) => alert(genre)}
-    //             selected="comedy"
-    //         />
-    //     </Container>
-    //     <Container>
-    //         <SortControl selectedOption="release date" options={["release date", "title"]} onSelect={(option) => alert(option)} />
-    //     </Container>
-    //     <MovieDetails movie={moviesArray[0]} />
-    //     <MovieContainer />
-    //     <MovieForm formTitle="Add movie" submitCallback={(formData) => console.warn(formData)} initialMovie={moviesArray[0]} showModal={true}/>
-    // </div>
   );
 }
 
